f6cbd128eebb1a6a600b80d8a93076ee53888552
nhungmt1984-arch <nhungmt1984@gmail.com>
2026-01-30 16:15:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -191,7 +191,7 @@ st.markdown("""
 st.markdown("""
 <div class="main-header">
     <h1>📘 TRỢ LÝ SOẠN GIÁO ÁN TỰ ĐỘNG (NLS)</h1>
-    <p>Tác giả: Nông Thị Nhung - Trường PTDTBT Tiểu học Kiên Đài - ĐT: 0972 982 566</p>
+    <p>Tác giả: Nông Thị Nhung - Trường PTDTBT Tiểu học Kiên Đài - ĐT: 097 2982 566</p>
 </div>
 """, unsafe_allow_html=True)
 
@@ -347,4 +347,4 @@ if st.button("🚀 SOẠN GIÁO ÁN NGAY"):
 
 # --- CHÂN TRANG ---
 st.markdown("---")
-st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Nông Thị Nhung - Trường PTDTBT Tiểu học Kiên Đài - ĐT: 097 1986 343</div>", unsafe_allow_html=True)
+st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Nông Thị Nhung - Trường PTDTBT Tiểu học Kiên Đài - ĐT: 097 2982 566</div>", unsafe_allow_html=True)
